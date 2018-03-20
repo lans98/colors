373b45938fb81c7a8802bc4fbcb807455d66604a
lans98 <lans9831@gmail.com>
2018-03-20 11:54:50 -0500

    added gradient.hpp testing gradients

--- gradient.hpp
+++ gradient.hpp
@@ -1,11 +1,50 @@
 #ifndef COLORS_GRADIENT_HPP
 #define COLORS_GRADIENT_HPP 
 
+#include <cmath>
+
 #include "color.hpp"
+
 
 namespace color {
 
+double linear_interpolation(double a, double b, double step) {
+  return (1 - step) * a + step * b;
+}
 
+RGB gradient(const RGB& p, const RGB& q) {
+  double r, g, b;
+
+  r = linear_interpolation(p.get_r(), q.get_r(), 0.5);
+  g = linear_interpolation(p.get_g(), q.get_g(), 0.5);
+  b = linear_interpolation(p.get_b(), q.get_b(), 0.5);
+
+  r = std::round(r);
+  g = std::round(g);
+  b = std::round(b);
+
+  return RGB(static_cast<unsigned char>(r), static_cast<unsigned char>(g), static_cast<unsigned char>(b));
+}
+
+HSV gradient(const HSV& a, const HSV& b) {
+  double h, s, v;
+
+  h = linear_interpolation(a.get_h(), b.get_h(), 0.5);
+  s = linear_interpolation(a.get_s(), b.get_s(), 0.5);
+  v = linear_interpolation(a.get_v(), b.get_v(), 0.5);
+
+  return HSV(h, s, v);
+}
+
+HSL gradient(const HSL& a, const HSL& b) {
+  double h, s, l;
+
+  h = linear_interpolation(a.get_h(), b.get_h(), 0.5);
+  s = linear_interpolation(a.get_s(), b.get_s(), 0.5);
+  l = linear_interpolation(a.get_l(), b.get_l(), 0.5);
+
+  return HSL(h, s, l);
+}
 
 
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "color.hpp"
+#include "gradient.hpp"
 
 int main() {
   color::RGB color1("#231d2f");
@@ -20,6 +21,24 @@ int main() {
 
   color::RGB color6(color5);
   std::cout << color6 << std::endl;
+
+
+  // Testing gradient
+  color::RGB colora_rgb("#4100e5");
+  color::RGB colorb_rgb("#0300bf");
+  auto gradient_rgb = color::gradient(colora_rgb, colorb_rgb);
+  std::cout << "gradient rgb: " << gradient_rgb << std::endl;
+
+
+  color::HSV colora_hsv(colora_rgb);
+  color::HSV colorb_hsv(colorb_rgb);
+  auto gradient_hsv = color::gradient(colora_hsv, colorb_hsv);
+  std::cout << "gradient hsv: " << gradient_hsv << std::endl;
+
+  color::HSL colora_hsl(colora_rgb);
+  color::HSL colorb_hsl(colorb_rgb);
+  auto gradient_hsl = color::gradient(colora_hsl, colorb_hsl);
+  std::cout << "gradient hsl: " << gradient_hsl << std::endl;
 
   return 0;
 }
